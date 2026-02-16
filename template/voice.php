b18ea9288cd14603dc628b1a65b147b442b93c6b
bac7220 <bacbac7220@gmail.com>
2026-02-16 10:06:38 +0900

    voiceページSCFの修正に対応 スタイリングをこれからします

--- template/voice.php
+++ template/voice.php
@@ -9,18 +9,36 @@
 
           <div class="p-voice__container">
             <div class="p-voice__container-inner">
-              
+              <div class="p-voice__items">
+                <?php
+                $voice_group = SCF::get('voice_group', 272);
+                if ($voice_group):
+                  foreach ($voice_group as $voice_item):
+                    $img = $voice_item['voice_img'];
+                    $img_url = wp_get_attachment_image_url($img, 'full');
+                    $text = $voice_item['voice_text']; ?>
+                    <div class="p-voice__item">
+                      <div class="p-voice__img">
+                        <img src="<?php echo esc_url($img_url); ?>" alt="">
+                      </div>
+                      <div class="p-voice__text-box">
+                        <p class="p-voice__text">
+                          <?php echo nl2br(esc_html($text)) ?>
+                        </p>
+                      </div>
+                    </div>
+                  <?php endforeach; ?>
+                <?php endif; ?>
+              </div>
             </div>
           </div>
-         
-        </div>
-        <div class="l-button ">
-          <a href="<?php echo esc_url(home_url('/about')); ?>" class="c-button ">
-            もっと見る <svg class="c-button__icon" width="24" height="24" role="img" aria-label="right-arrow">
-              <use href="#right-svg"></use>
-            </svg>
-          </a>
+          <div class="l-button ">
+            <a href="<?php echo esc_url(home_url('/about')); ?>" class="c-button ">
+              もっと見る <svg class="c-button__icon" width="24" height="24" role="img" aria-label="right-arrow">
+                <use href="#right-svg"></use>
+              </svg>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
   </section>
