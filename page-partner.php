d86a0f4abf7483c3709230b99f137b276c1ed2a6
bac7220 <bacbac7220@gmail.com>
2026-03-05 16:28:23 +0900

    パートナー対応ページ作成開始

---
+++ page-partner.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+
+
+<section class="partner-fv">
+  
+</section>
+
+<section class="skill">
+
+</section>
+
+<section class="role">
+
+</section>
+
+<section class="price">
+
+</section>
+
+<section class="flow">
+
+</section>
+
+<section class="contact">
+
+</section>
+
+<?php get_template_part('template/footer-item'); ?>
+
+
+<?php get_template_part('template/svg'); ?>
+
+
+<?php get_footer(); ?>
