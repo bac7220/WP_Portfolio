75c41ea8bbab3da42ce7e574215b8ef8c0850be1
bac7220 <bacbac7220@gmail.com>
2026-05-04 21:58:44 +0900

    worksまで微調整

--- template/partner/partner-works-slider.php
+++ template/partner/partner-works-slider.php
@@ -67,17 +67,9 @@
             </ul>
         </div>
 
-        <!-- ナビ矢印（オレンジ円） -->
-        <button type="button" class="p-partner-works__nav p-partner-works__nav--prev" aria-label="前へ">
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" fill="currentColor"/>
-            </svg>
-        </button>
-        <button type="button" class="p-partner-works__nav p-partner-works__nav--next" aria-label="次へ">
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" fill="currentColor"/>
-            </svg>
-        </button>
+        <!-- ナビ矢印（オレンジ円・三角形） -->
+        <button type="button" class="p-partner-works__nav p-partner-works__nav--prev" aria-label="前へ"></button>
+        <button type="button" class="p-partner-works__nav p-partner-works__nav--next" aria-label="次へ"></button>
     </div>
 
     <div class="p-partner-works__inner">
